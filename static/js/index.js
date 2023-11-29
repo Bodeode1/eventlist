c7090f8bdf8723d5d4ae00b74808008451646049
Bodeode1 <bodeode2000@yahoo.com>
2023-11-29 09:24:54 +0000

    Make Closing Modal to be Conditional

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,9 @@
 const selectAll = e =>document.querySelectorAll(e)
 const selector = e => document.querySelector(e)
 const modalCloser = selector(".modal-closer")
-modalCloser.addEventListener("click", e => {
-    const modalBox = selector(".modal")
-    modalBox.setAttribute("class", "d-none")
-})
+if(modalCloser){
+    modalCloser.addEventListener("click", e => {
+        const modalBox = selector(".modal")
+        modalBox.setAttribute("class", "d-none")
+    })
+}
